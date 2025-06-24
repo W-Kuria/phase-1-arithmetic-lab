0431aec0a8bbcf7b4a9286a2780a119603070ed6
W-Kuria <william.njoroge.kuria@gmail.com>
2025-06-24 12:23:57 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 
 // Write your code here
+
+
+// 1. Create a variable called `multiply` set to an equation that will multiply the
+//    variables `num1` and `num2`; the result of the multiplication should be `62`.
+// 2. Create a variable called `random` that will generate a random integer greater
+//    than 0.
+// 3. Create a variable called `mod` set to an equation that will calculate the
+//    remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+// 4. Create a variable called `max` that finds the highest number in a set; the
+//    value returned should be `20`
+ 
+
+//Task 1
+// function multiply(num1, num2){
+// let num1 = 31;
+// let num2 = 2;
+// let multiply = num1 * num2;
+
+// console.log(multiply);
+// }
+
+//Task 1
+let num1=31
+let num2=2
+let multiply=num1*num2
+
+//Task 2
+let random = Math.floor(Math.random() ) + 10;
+
+//Task 3
+const num3 = 16;
+const num4 = 6;      
+let mod = num3 % num4;
+
+//Task 4
+let max = Math.max(5, 10, 15, 20);
+
+
+
+
